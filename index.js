0b55bed6b34c558c0f032bc9d4f30b6599c79938
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-02-18 14:49:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
+
 document.addEventListener('DOMContentLoaded', function() {
     const paragraphs = localStorage.getItem('typedText') || "";
 
-    document.getElementById('waterform').addEventListener('submit', function(event) {
+    document.getElementById('submitBtn').addEventListener('click', function(event) {
         event.preventDefault(); 
-        const typedText = document.getElementById('message').value; 
+        
+        
+        const typedText = document.getElementById('message').value.trim();
+        if (typedText === "") {
+            
+            alert("Please enter some text before proceeding.");
+            return; 
+        }
+        
+       
+        document.getElementById('message').setAttribute('required', '');
+        
         localStorage.setItem('typedText', typedText); 
-        window.location.href = 'https://charansaiparvathala.github.io/TypingGame/TypingGame.html';
+        window.location.href = 'file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/TypingGame.html';
+    });
+});
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const paragraphs = localStorage.getItem('typedText') || "";
+
+    document.getElementById('Btn2').addEventListener('click', function(event) {
+        event.preventDefault(); 
+        
+        
+        const typedText = document.getElementById('message').value.trim();
+        if (typedText === "") {
+            
+            alert("Please enter some text before proceeding.");
+            return; 
+        }
+        
+       
+        document.getElementById('message').setAttribute('required', '');
+        
+        localStorage.setItem('typedText', typedText); 
+        window.location.href = 'file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/Entry.html';
     });
 });
