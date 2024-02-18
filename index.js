95e17f065705d4c313d60bfb0bba105970bc4be5
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-02-18 14:55:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,8 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-    const paragraphs = localStorage.getItem('typedText') || "";
+        document.getElementById("Btn1").addEventListener("click", function() {
+            window.location.href = "file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/RandText.html";
+        });
 
-    document.getElementById('submitBtn').addEventListener('click', function(event) {
-        event.preventDefault(); 
-        
-        
-        const typedText = document.getElementById('message').value.trim();
-        if (typedText === "") {
-            
-            alert("Please enter some text before proceeding.");
-            return; 
-        }
-        
-       
-        document.getElementById('message').setAttribute('required', '');
-        
-        localStorage.setItem('typedText', typedText); 
-        window.location.href = 'file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/TypingGame.html';
-    });
-});
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const paragraphs = localStorage.getItem('typedText') || "";
-
-    document.getElementById('Btn2').addEventListener('click', function(event) {
-        event.preventDefault(); 
-        
-        
-        const typedText = document.getElementById('message').value.trim();
-        if (typedText === "") {
-            
-            alert("Please enter some text before proceeding.");
-            return; 
-        }
-        
-       
-        document.getElementById('message').setAttribute('required', '');
-        
-        localStorage.setItem('typedText', typedText); 
-        window.location.href = 'file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/Entry.html';
-    });
-});
+        document.getElementById("Btn2").addEventListener("click", function() {
+            window.location.href = "file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/imdex.html"; // Replace "page2.html" with your desired URL
+        });
