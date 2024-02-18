76a4ba6eefaefc159aa8991c4656d2b55beab947
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-02-18 15:04:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
         document.getElementById("Btn1").addEventListener("click", function() {
-            window.location.href = "file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/RandText.html";
+            window.location.href = "https://charansaiparvathala.github.io/TypingGame/Rand.html";
         });
 
         document.getElementById("Btn2").addEventListener("click", function() {
-            window.location.href = "file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/imdex.html"; // Replace "page2.html" with your desired URL
+            window.location.href = "https://charansaiparvathala.github.io/TypingGame/Custom.html"; // Replace "page2.html" with your desired URL
         });
