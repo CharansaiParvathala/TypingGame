9aa2c5b11786c8b888fea0af6090187731dc8af2
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-02-18 14:54:20 +0530

    Update

---
+++ Custom.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const paragraphs = localStorage.getItem('typedText') || "";
+
+    document.getElementById('submitBtn').addEventListener('click', function(event) {
+        event.preventDefault(); 
+
+
+        const typedText = document.getElementById('message').value.trim();
+        if (typedText === "") {
+
+            alert("Please enter some text before proceeding.");
+            return; 
+        }
+
+
+        document.getElementById('message').setAttribute('required', '');
+
+        localStorage.setItem('typedText', typedText); 
+        window.location.href = 'file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/TypingGame.html';
+    });
+});
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const paragraphs = localStorage.getItem('typedText') || "";
+
+    document.getElementById('Btn2').addEventListener('click', function(event) {
+        event.preventDefault(); 
+
+
+        const typedText = document.getElementById('message').value.trim();
+        if (typedText === "") {
+
+            alert("Please enter some text before proceeding.");
+            return; 
+        }
+
+
+        document.getElementById('message').setAttribute('required', '');
+
+        localStorage.setItem('typedText', typedText); 
+        window.location.href = 'file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/Entry.html';
+    });
+});
