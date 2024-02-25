0b50db084018dffcf75c9ac3901252bd68e93fd2
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-02-25 16:19:09 +0530

    Update mode.js

--- mode.js
+++ mode.js
@@ -1,12 +1,9 @@
 document.getElementById("Btn1").addEventListener("click", function() {
 const audio = new Audio();
-         audio.src = "https://res.cloudinary.com/dkh9qnxkd/video/upload/v1708856768/mouse-click-153941_dlwpdq.mp3";
-audio.play();
+         
             window.location.href = "https://charansaiparvathala.github.io/TypingGame/Rand.html";
         });
 
         document.getElementById("Btn2").addEventListener("click", function() {
-audio.src = "https://res.cloudinary.com/dkh9qnxkd/video/upload/v1708856768/mouse-click-153941_dlwpdq.mp3";
-audio.play();
             window.location.href = "https://charansaiparvathala.github.io/TypingGame/Custom.html";
         });
