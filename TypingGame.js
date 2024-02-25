0dd52fd465368aad90bef24f2274feba2283081e
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-02-25 16:16:20 +0530

    Update TypingGame.js

--- TypingGame.js
+++ TypingGame.js
@@ -90,8 +90,6 @@ function initTimer() {
 }
 
 function resetGame() {
-audio.src = "https://res.cloudinary.com/dkh9qnxkd/video/upload/v1708856768/mouse-click-153941_dlwpdq.mp3";
-audio.play();
     loadParagraph();
     clearInterval(timer);
     timeLeft = maxTime;
