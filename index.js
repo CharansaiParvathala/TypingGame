843e8187373760621dd4423d8fc4a74ac8e2a909
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-02-25 17:24:03 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-
-        document.getElementById("Btn1").addEventListener("click", function() {
-            window.location.href = "https://charansaiparvathala.github.io/TypingGame/Rand.html";
-        });
-
-        document.getElementById("Btn2").addEventListener("click", function() {
-            window.location.href = "https://charansaiparvathala.github.io/TypingGame/Custom.html"; // Replace "page2.html" with your desired URL
-        });
