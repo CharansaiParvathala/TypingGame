26da4f251522e3b5e32d2edf405eb2faf64f5c13
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-02-26 12:40:08 +0530

    Update mode.js

--- mode.js
+++ mode.js
@@ -1,9 +1,9 @@
 document.getElementById("Btn1").addEventListener("click", function() {
 const audio = new Audio();
          
-            window.location.href = "https://charansaiparvathala.github.io/TypingGame/Rand.html";
+            window.location.href = "Rand.html";
         });
 
         document.getElementById("Btn2").addEventListener("click", function() {
-            window.location.href = "https://charansaiparvathala.github.io/TypingGame/Custom.html";
+            window.location.href = "Custom.html";
         });
