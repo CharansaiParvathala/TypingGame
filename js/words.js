32739520580cbe17abb3c2f91ceb30c436b8d147
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-03-24 12:52:31 +0530

    Update js

---
+++ js/words.js
@@ -0,0 +1,134 @@
+const words = [
+    "if",
+    "do",
+    "as",
+    "or",
+    "in",
+    "on",
+    "if",
+    "is",
+    "for",
+    
+    // Three characters
+    "def",
+    "try",
+    "else",
+    "elif",
+    "int",
+    "new",
+    "var",
+    "let",
+    "const",
+
+    // Four characters
+    "void",
+    "case",
+    "else",
+    "this",
+    "true",
+    "null",
+    "echo",
+    "echo",
+    "true",
+    "null",
+    "self",
+    "echo()",
+    "elif",
+    "do()",
+    "while",
+
+    // Five characters
+    "class",
+    "throw",
+    "while",
+    "break",
+    "print",
+    "print",
+    "false",
+    "empty",
+    "print()",
+    "false",
+    "empty",
+
+    // Six characters
+    "static",
+    "return",
+    "import",
+    "elseif",
+    "return",
+    "elseif",
+    "public",
+    "String",
+    "parent",
+    "parent",
+    "return",
+
+    // Seven characters
+    "private",
+    "default",
+    "finally",
+    "include",
+    "require",
+    "include",
+    "require",
+    "abstract",
+    "function",
+
+    // Eight characters
+    "continue",
+    "foreach",
+    "interface",
+    "implements",
+    "continue",
+    "continue",
+
+    // Nine characters
+    "protected",
+    "interface",
+    "implements",
+
+    // Ten characters
+    "implements",
+
+    // Eleven characters
+    "do while()",
+    "System.out",
+    "System.out",
+    "System.out",
+    "System.out",
+    "System.out",
+
+    // Twelve characters
+    "ArrayList.add",
+    "ArrayList.get",
+    "ArrayList.get",
+    "ArrayList.add",
+
+    // Thirteen characters
+    "Scanner.nextInt",
+    "Scanner.nextInt",
+    "Scanner.nextFloat",
+    "Scanner.nextFloat",
+
+    // Fourteen characters
+    "Scanner.nextLine",
+    "Scanner.nextLine",
+    "Math.random()",
+    "Math.random()",
+
+    // Fifteen characters
+    "String.valueOf",
+    "String.valueOf",
+
+    // Sixteen characters
+    "ArrayList.isEmpty",
+    "ArrayList.isEmpty",
+
+    // Seventeen characters
+    "ArrayList.clear",
+    "ArrayList.clear",
+
+    // Eighteen characters
+    "ArrayList.contains",
+    "ArrayList.contains",
+];
